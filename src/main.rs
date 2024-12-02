751d95b348cb1b94c93043c4b50cf1ae6cef1182
Mason Pike <mason.pike@emeria.eu>
2024-12-02 20:38:20 +0100

    1.2 1.2 1.2 1.2 1.2 1.2 1.2 1.2 1.2 1.2 1.2 1.2 1.2 1.2 1.2 1.2 1.2 1.2 1.2 1.2 1.2

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,8 @@ fn main() {
     let start = Instant::now();
 
     let solution = match input.trim() {
-        "01" => solutions::_01::solve("src/solutions/_01/_01.txt").to_string(),
+        "1.1" => solutions::_01::part_1("src/solutions/_01/_01.txt").to_string(),
+        "1.2" => solutions::_01::part_2("src/solutions/_01/_01.txt").to_string(),
         &_ => "Not valid".to_string(),
     };
 
--- src/solutions/_01/mod.rs
+++ src/solutions/_01/mod.rs
@@ -1,6 +1,27 @@
+use std::collections::HashMap;
+
 use super::utils::parse_file_line_by_line;
 
-pub(crate) fn solve(path: &str) -> i32 {
+pub(crate) fn part_1(path: &str) -> i32 {
+    let (mut left, mut right) = get_vecs(path);
+    left.sort();
+    right.sort();
+    left.into_iter()
+        .zip(right)
+        .fold(0, |acc, (l, r)| acc + l.abs_diff(r) as i32)
+}
+
+pub(crate) fn part_2(path: &str) -> i32 {
+    let (left, right) = get_vecs(path);
+    let mut occurences: HashMap<i32, i32> = HashMap::new();
+    for r in right {
+        *occurences.entry(r).or_default() += 1;
+    }
+    left.into_iter()
+        .fold(0, |acc, l| acc + occurences.get(&l).unwrap_or(&0) * l)
+}
+
+fn get_vecs(path: &str) -> (Vec<i32>, Vec<i32>) {
     let mut left: Vec<i32> = vec![];
     let mut right: Vec<i32> = vec![];
     parse_file_line_by_line(path, |line| {
@@ -13,12 +34,7 @@ pub(crate) fn solve(path: &str) -> i32 {
         });
     })
     .unwrap();
-    left.sort();
-    right.sort();
-    left.into_iter().zip(right).fold(0, |acc, (l, r)| {
-        println!("{:?}, {:?}", l, r);
-        acc + l.abs_diff(r) as i32
-    })
+    (left, right)
 }
 
 #[cfg(test)]
@@ -27,6 +43,11 @@ mod tests {
 
     #[test]
     fn test_01_part_1() {
-        assert_eq!(solve("src/solutions/_01/_01.test.txt"), 11)
+        assert_eq!(part_1("src/solutions/_01/_01.test.txt"), 11)
+    }
+
+    #[test]
+    fn test_01_part_2() {
+        assert_eq!(part_2("src/solutions/_01/_01.test.txt"), 31)
     }
 }
